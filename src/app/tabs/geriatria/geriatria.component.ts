eb21639309746258fa81a4346df4d683aa2458f8
Daniel Florido <d.florido@javeriana.edu.co>
2024-07-30 14:23:41 -0500

    add navbar

---
+++ src/app/tabs/geriatria/geriatria.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-geriatria',
+  standalone: true,
+  imports: [],
+  templateUrl: './geriatria.component.html',
+  styleUrl: './geriatria.component.css'
+})
+export class GeriatriaComponent {
+
+}
