dd8caa86cd96012ed09091f85524e55a40ec996e
Daniel Florido <d.florido@javeriana.edu.co>
2024-08-16 10:38:37 -0500

    navbar works

--- src/app/header/navbar/navbar/navbar.component.ts
+++ src/app/header/navbar/navbar/navbar.component.ts
@@ -1,21 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   items = [
-    { label: 'Geriatria', icon : 'Ger'},
-    { label: 'Psiquiatria', icon : 'Ger'},
-    { label: 'Neurologia',  icon : 'Ger'},
-    { label: 'Neuropsicologia', icon : 'Ger'},
-    { label: 'Conclusiones Conjuntas', icon : 'Ger'},
-    { label: 'Informe', icon : 'Ger'}
+    { label: 'Informacion General', icon : 'Ger', route: 'infogeneral'},
+    { label: 'Geriatria', icon : 'Ger', route: 'geriatria'},
+    { label: 'Psiquiatria', icon : 'Ger', route: 'psiquiatria'},
+    { label: 'Neurologia',  icon : 'Ger', route: 'neurologia'},
+    { label: 'Neuropsicologia', icon : 'Ger', route: 'neuropsicologia'},
+    { label: 'Conclusiones Conjuntas', icon : 'Ger', route: 'conclusiones'},
+    { label: 'Informe', icon : 'Ger', route: 'informe'}
   ];
 }
