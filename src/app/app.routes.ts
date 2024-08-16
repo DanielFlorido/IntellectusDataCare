64a9f2e9cf8ec75c1fb7fd6b851f1b1df3dc152a
Daniel Florido <d.florido@javeriana.edu.co>
2024-08-16 08:52:40 -0500

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { GeriatriaComponent } from './tabs/geriatria/geriatria.component';
+import { NeurologiaComponent } from './tabs/neurologia/neurologia.component';
+import { NeuropsicologiaComponent } from './tabs/neuropsicologia/neuropsicologia.component';
+import { PsiquiatriaComponent } from './tabs/psiquiatria/psiquiatria.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '/geriatria',
+        component: GeriatriaComponent,
+        title: 'Geriatria'  
+    },
+    {
+        path: '/neurologia',
+        component: NeurologiaComponent,
+        title: 'Neurologia'  
+    },
+    {
+        path: '/neuropsicologia',
+        component: NeuropsicologiaComponent,
+        title: 'Neuropsicologia'  
+    },
+    {
+        path: '/psiquiatria',
+        component: PsiquiatriaComponent,
+        title: 'Psiquiatria'  
+    },
+    {
+        path: '/informe',
+        component: GeriatriaComponent,
+        title: 'Informe'  
+    },
+    {
+        path: '/geriatria',
+        component: GeriatriaComponent,
+        title: 'Geriatria'  
+    },
+];
