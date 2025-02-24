ebde67eb8a28c3e8b682f7b68927b17832bd7177
Daniel Florido <d.florido@javeriana.edu.co>
2025-02-24 13:26:00 -0500

    `Added FormularioComponent and updated neu-form-1 component to use it, added questions and onSubmit handler, and added neu-form-1 component to neurologia component`

--- src/app/forms/neurologiaForms/neu-form-1/neu-form-1.component.ts
+++ src/app/forms/neurologiaForms/neu-form-1/neu-form-1.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormularioComponent } from "../../formulario/formulario.component";
+import { FormGroup, Validators } from '@angular/forms';
+import { Question } from '../../../interfaces/question';
 
 @Component({
   selector: 'app-neu-form-1',
   standalone: true,
-  imports: [],
+  imports: [FormularioComponent],
   templateUrl: './neu-form-1.component.html',
   styleUrl: './neu-form-1.component.css'
 })
 export class NeuForm1Component {
-
+  questions: Question[] = [
+    { id: '1', label: 'MMSE', type: 'number', validators: [Validators.required, Validators.min(0), Validators.max(30)] },
+    { id: '2', label: 'MOCA', type: 'number', validators: [Validators.required, Validators.min(0), Validators.max(30)] },
+    { id: '3', label: 'Figura del reloj', type: 'number', validators: [Validators.required,Validators.min(0), Validators.max(9)] },
+    { id: '4', label: 'CDR', type: 'number', validators: [Validators.required,Validators.min(0), Validators.max(3)] }
+  ];
+  onSubmit(data: FormGroup) {
+    console.log(data.value);
+  }
 }
--- src/app/tabs/neurologia/neurologia.component.ts
+++ src/app/tabs/neurologia/neurologia.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NeuIsquemiaHachinskiComponent } from "../../forms/neurologiaForms/neu-isquemia-hachinski/neu-isquemia-hachinski.component";
+import { NeuForm1Component } from "../../forms/neurologiaForms/neu-form-1/neu-form-1.component";
+import { NeuIfsComponent } from "../../forms/neurologiaForms/neu-ifs/neu-ifs.component";
 
 @Component({
   selector: 'app-neurologia',
   standalone: true,
-  imports: [NeuIsquemiaHachinskiComponent],
+  imports: [NeuIsquemiaHachinskiComponent, NeuForm1Component, NeuIfsComponent],
   templateUrl: './neurologia.component.html',
   styleUrl: './neurologia.component.css'
 })
