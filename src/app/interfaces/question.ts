2c6d109587f17d881cbab9d166a3a203c9624cff
Daniel Florido <d.florido@javeriana.edu.co>
2025-03-23 20:53:54 -0500

    `Removed 'number' type from Question interface type enum`

--- src/app/interfaces/question.ts
+++ src/app/interfaces/question.ts
@@ -3,7 +3,7 @@ import { ValidatorFn } from "@angular/forms";
 export interface Question {
     id: string;
     label:string;
-    type: 'text' | 'number' | 'radio' | 'select' | 'number'| 'date' | 'yesNo'| 'selectValue';
+    type: 'text' | 'number' | 'radio' | 'select' | 'date' | 'yesNo'| 'selectValue';
     options?: string[];
     optionsValue?: {label:string; value: any}[];
     value?: any;
